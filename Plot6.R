963d9b93981e65f5aa07cb760002e343e39201ec
Massimiliano Figini <massy@ymail.com>
2016-05-26 16:52:41 +0200

    Update Plot6.R

--- Plot6.R
+++ Plot6.R
@@ -1,5 +1,3 @@
-setwd("C:/Users/figinim/Documents/R")
-
 # 6. Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources in Los Angeles 
 #    County, California (fips == "06037"). Which city has seen greater changes over time in motor vehicle emissions?
 
